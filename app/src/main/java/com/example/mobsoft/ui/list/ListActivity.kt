f61a32ee32b52c07b82c0897ba3ad8f292ebc58e
patrikkarolyi <patrikkarolyi@hotmail.com>
2019-03-30 11:15:14 +0100

    list viewPager for fragments

--- app/src/main/java/com/example/mobsoft/ui/list/ListActivity.kt
+++ app/src/main/java/com/example/mobsoft/ui/list/ListActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.mobsoft.R
 import com.example.mobsoft.injector
+import com.example.mobsoft.ui.list.adapaters.ListPagerAdapter
+import kotlinx.android.synthetic.main.activity_list.*
 import javax.inject.Inject
 
 class ListActivity : AppCompatActivity(), ListScreen {
@@ -13,7 +15,7 @@ class ListActivity : AppCompatActivity(), ListScreen {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_list)
         injector.inject(this)
     }
 
@@ -25,5 +27,13 @@ class ListActivity : AppCompatActivity(), ListScreen {
     override fun onStop() {
         super.onStop()
         presenter.detachScreen()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        listViewPager.adapter =
+            ListPagerAdapter(baseContext!!, supportFragmentManager!!)
+        listSlidingTabs.setupWithViewPager(listViewPager)
+
     }
 }
